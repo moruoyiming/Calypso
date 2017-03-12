152143e3ab5ed06ccf602df4d69c9dd06b240861
jianruilin <798774875@qq.com>
2017-03-12 21:25:54 +0800

    add file

---
+++ app/src/main/java/com/calypso/library/MainActivity.java
@@ -0,0 +1,13 @@
+package com.calypso.library;
+
+import android.app.Activity;
+
+/**
+ * Project：Rxjava
+ * Created：jianz
+ * Date：2017/3/12 21:12
+ * Summry：
+ */
+
+public class MainActivity extends Activity {
+}
