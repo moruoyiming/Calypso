a1ce6d4a2c01b2599aec35351d294288c30d6c23
jianruilin <798774875@qq.com>
2017-04-27 10:55:40 +0800

    add FlycoTabLayout

---
+++ calypsolib/src/main/java/com/calypso/library/view/FlycoTabLayout/listener/CustomTabEntity.java
@@ -0,0 +1,13 @@
+package com.calypso.library.view.FlycoTabLayout.listener;
+
+import android.support.annotation.DrawableRes;
+
+public interface CustomTabEntity {
+    String getTabTitle();
+
+    @DrawableRes
+    int getTabSelectedIcon();
+
+    @DrawableRes
+    int getTabUnselectedIcon();
+}
---
+++ calypsolib/src/main/java/com/calypso/library/view/FlycoTabLayout/listener/OnTabSelectListener.java
@@ -0,0 +1,6 @@
+package com.calypso.library.view.FlycoTabLayout.listener;
+
+public interface OnTabSelectListener {
+    void onTabSelect(int position);
+    void onTabReselect(int position);
+}
